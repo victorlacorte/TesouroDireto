ef0ef0c810a952d05b1ab66242ede056670021e3
victorlacorte <victorgabriel89@gmail.com>
2017-11-17 10:29:59 -0200

    Included effective interest rate tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,4 @@
-from tdcalc.helpers import truncate, get_bizcalendar
+from tdcalc.helpers import truncate, get_bizcalendar, eff_interest_rate
 
 
 def test_truncate():
@@ -12,3 +12,11 @@ def test_bizcalendar():
     cal = get_bizcalendar()
     assert cal.isbizday('2014-01-13')
     assert cal.bizdays('2014-01-13', '2015-01-13') == 253
+
+def test_eff_interest_rate():
+    i1 = 0.1
+    n1 = 12
+    assert round(eff_interest_rate(i1, n1), 4) == 0.1047
+    i2 = 0.101
+    n2 = 2
+    assert round(eff_interest_rate(i2, n2), 4) == 0.1036
