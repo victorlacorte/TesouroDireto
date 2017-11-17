4e1edacad9071c17c416d82b11bd219f888a5ddf
victorlacorte <victorgabriel89@gmail.com>
2017-11-17 10:29:13 -0200

    Included effective interest rate

--- tdcalc/helpers.py
+++ tdcalc/helpers.py
@@ -3,7 +3,7 @@ from bizdays import Calendar, load_holidays
 
 def truncate(x, n):
     '''
-        Return x truncated to n decimal places
+        Return x truncated to n decimal places.
     '''
     # https://stackoverflow.com/questions/20544714/truncate-a-decimal-value-in-python
     # Possibly also:
@@ -16,5 +16,17 @@ def truncate(x, n):
         return math.floor(x * p) / p
 
 def get_bizcalendar(holidays_file='tdcalc/data/anbima.txt'):
+    '''
+        Return a bizdays Calendar instance initialized with ANBIMA brazilian
+           holidays.
+    '''
     holidays = load_holidays(holidays_file)
     return Calendar(holidays, ['Sunday', 'Saturday'], name='Brazil')
+
+# https://www.investopedia.com/terms/e/effectiveinterest.asp
+def eff_interest_rate(i, n):
+    '''
+        Return the effective interest rate of a nominal rate i and the number
+            n of compounding periods.
+    '''
+    return pow(1 + i / n, n) - 1
